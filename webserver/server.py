8f4c8ed30e3577d8b0baf770191ab6d64616dcd7
JoshuaUhlfelder <117771779+JoshuaUhlfelder@users.noreply.github.com>
2022-11-10 03:15:20 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -150,11 +150,11 @@ def create_new_account():
         cashapp = request.form['cashapp']
         image = request.form['image']
         cmd = 'INSERT INTO Users VALUES (:email1), (:fullname1), (:uni1), (:password1), (:venmo1), (:cashapp1), (:image1)';
+        print(cmd)
         c = g.conn.execute(text(cmd), email1 = email, fullname1 = fullname, uni1 = uni, password1 = password, venmo1 = venmo, cashapp1 = cashapp, image1 = image);
         c.close()
     except:
         flash('Error creating account! Ensure all fields are entered correctly.')
-        return redirect('/newaccount')
     return home()
 
 ##############################

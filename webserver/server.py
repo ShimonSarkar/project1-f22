fdd746809dcbd004d814a114e34a9e65e9e6bd45
ShimonSarkar <67527458+ShimonSarkar@users.noreply.github.com>
2022-11-12 23:39:44 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -262,6 +262,8 @@ def message():
     args = request.args
     uid = args.get("uid")
     pholder = args.get("pholder")
+    if pholder != '':
+        pholder = "Hi, I would like to ask about the following product" + pholder
     cmd = 'SELECT * FROM Messages_Sent_Received WHERE sender_email = (:sender1) AND receiver_email = (:sender2) OR sender_email = (:sender2) AND receiver_email = (:sender1) ORDER BY date_created, time_created';
     c = g.conn.execute(text(cmd), sender1 = session['email'], sender2 = uid);
     messages = c.fetchall()

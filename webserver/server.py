3d15de2e4c04a1b2dd9e6b09016b7de62409f78f
JoshuaUhlfelder <117771779+JoshuaUhlfelder@users.noreply.github.com>
2022-11-14 15:24:31 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -724,7 +724,7 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=False, threaded=threaded)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded, use_reloader=False)
 
 
   run()
